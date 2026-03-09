5c5e784d9bb86fef0b232362379aaffda9f4287c
EstebanPastranaCarmona <esteban.pastrana@ulatina.net>
2026-03-09 11:55:40 -0600

    Epastrana, se creo la primera version del sistema

--- Project/app/users/admin.py
+++ Project/app/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'get_rol', 'is_active')
+    list_filter = ('groups', 'is_active')
+    search_fields = ('username', 'email')
+
+    def get_rol(self, obj):
+        return obj.get_rol()
+    get_rol.short_description = 'Rol'
+
